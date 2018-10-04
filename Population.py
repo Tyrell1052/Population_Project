21e025e9c1880deb7e334366466a35b9021dd46c
Tyrell <Tyrell1052@gmail.com>
2018-10-04 02:10:51 -0400

    added more functionality to the datetime i imported earlier

--- Population.py
+++ Population.py
@@ -1,7 +1,18 @@
 """"
 Program: Population.py
 Author: Tyrell Robbins
-Simulate 3 Organisms lifespan """
+This program will Simulate the life cycle of 3 Organisms
+based on growth rates, death rates and predictions over
+the course of 5 years """
+
+# import datetime for adding time in weeks and years
+
+import datetime
+
+# Adding today
+startDate = datetime.date.today()
+endDate = startDate
+endDate += datetime.timedelta(4)
 
 # Define Functions for
 def organismA():
@@ -13,3 +24,6 @@ def organismB():
 
 def organismC():
     StartingPopulation = 1000
+
+
+print(startDate)
