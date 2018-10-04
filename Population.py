056ddfeac33500ecf754dafda4b3afb646899516
Tyrell1052 <tyrell1052@gmail.com>
2018-10-03 23:00:18 -0400

    first commit, created functions for each organism.  trying to figure put how to add time in weeks into program

---
+++ Population.py
@@ -0,0 +1,15 @@
+""""
+Program: Population.py
+Author: Tyrell Robbins
+Simulate 3 Organisms lifespan """
+
+# Define Functions for
+def organismA():
+    startingPopulation = 50
+
+
+def organismB():
+    startingPopulation = 250
+
+def organismC():
+    StartingPopulation = 1000
