3e184574bfb8117526d8a7bc1dcc86c72b874890
Tyrell <Tyrell1052@gmail.com>
2018-10-05 01:40:34 -0400

    added more functionality in for the functions for each organism.

--- Population.py
+++ Population.py
@@ -10,27 +10,30 @@ the course of 5 years """
 import datetime
 
 # Adding today
-weeks = datetime.timedelta(weeks=1)
+WEEK = datetime.timedelta(weeks=1)
 
 
 # Define Functions for
-def organism_a():
-     while (weeks != 260):
+def organism_a(population, weeks):
+     #while weeks != 260:
 
-        population = 50
-        weeks = 0
+    population = 50
+    weeks = 0
 
-        if weeks == 4:
-            population * 2
+    if weeks < 4:
+        population * 2
 
-        elif weeks == 2:
-            population *.25
+    elif weeks < 2:
+        population *.25
 
-        elif weeks == 260:
-            print(population)
+    elif weeks == 260:
+        print(population)
 
-        else:
-            weeks += 1
+    else:
+        weeks += 1
+
+    return (population, weeks)
+
 
 
 
@@ -38,14 +41,14 @@ def organism_a():
 
 
 #def organismB():
-    startingPopulation = 250
+#    population = 250
 
 #def organismC():
-    StartingPopulation = 1000
+#    population = 1000
 
 
 #print(startDate)
 
 
 # main
-organism_a()
+#organism_a()
