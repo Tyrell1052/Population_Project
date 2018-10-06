e0772193f22d770f6524602a62acad413f2cb2e5
Tyrell1052 <tyrell1052@gmail.com>
2018-10-06 19:47:15 -0400

    testing out the logic.

--- Population.py
+++ Population.py
@@ -5,40 +5,42 @@ This program will Simulate the life cycle of 3 Organisms
 based on growth rates, death rates and predictions over
 the course of 5 years """
 
-# import datetime for adding time in weeks and years
-
-import datetime
-
-# Adding today
-#WEEK = datetime.timedelta(weeks=1)
-
 
 # Define Functions for
 # Display headers for table
 print("%4s%6s%12s%22s" %
       ("Organism A:", "Week", "Population", "1 million organisms",))
 
-population_a = 50
-weeks_a = 0
-weeks_in_years_a = 0
 
-print("%10s%15s" %
-      (weeks_in_years_a, population_a))
+def organism_a():
 
-while weeks_in_years_a < 260:
-    print(weeks_a, weeks_in_years_a)
-    if weeks_a == 2:
-        population_a = population_a - (population_a * .25)
-        print(population_a)
-    elif weeks_a == 4:
-        population_a = population_a * 2
-        print(population_a)
-        weeks_a = 0
-    elif population_a > 1000000:
-        print("Organism A will take",weeks_in_years_a,"weeks to reach 1 million organisms")
-        break
-    weeks_a += 1
-    weeks_in_years_a += 1
+    population = 50
+    weeks = 0
+    total_weeks = 0
 
+
+    print("%10s%15s" %
+          (total_weeks, population))
+
+    while total_weeks < 260:
+        print(weeks, total_weeks)
+        if weeks == 2:
+            population = population - (population * .25)
+            print(population)
+        elif weeks == 4:
+            population = population * 2
+            print(population)
+            weeks = 0
+        elif population > 1000000:
+            print("Organism A will take", total_weeks, "weeks to reach 1 million organisms.")
+            break
+        elif population == 0:
+            print("Organism A will take", total_weeks, "weeks until species reaches 0 organisms. ")
+            break
+        weeks += 1
+        total_weeks += 1
+
+
+organism_a()
 
 
--- test.py
+++ test.py
@@ -1,28 +1,37 @@
 
-print("%4s%6s%12s%22s" %
-      ("Organism A:", "Week", "Population", "1 million organisms",))
 
-population_a = 50
-weeks_a = 0
-weeks_in_years_a = 0
+def organism_a():
+    print("%4s%6s%12s" %
+          ("Organism A:", "Week", "Population",))
 
-#print("%10s%15s" %
-     # (weeks_in_years_a, population_a))
-
-while weeks_in_years_a < 260:
-    print(weeks_a, weeks_in_years_a)
-    if weeks_a == 2:
-        population_a = population_a - (population_a * .25)
-        print("%25s" % population_a)
-    elif weeks_a == 4:
-        population_a = population_a * 2
-        print(population_a)
-        weeks_a = 0
-    elif population_a > 1000000:
-        print("Organism A will take",weeks_in_years_a,"weeks to reach 1 million organisms")
-        break
-    weeks_a += 1
-    weeks_in_years_a += 1
+    population = 50
+    weeks = 0
+    total_weeks = 0
 
 
+   # print("%14s%15s" %
+          #(total_weeks, population))
 
+    while total_weeks < 260:
+        print("%14s%10s" %
+              (total_weeks, population))
+        if weeks == 2:
+            population = population - (population * .25)
+            print(population)
+        elif weeks == 4:
+            population = population * 2
+            weeks = 0
+
+        elif population > 1000000:
+            print("Organism A will take", total_weeks, "weeks to reach 1 million organisms.")
+            break
+
+        elif population == 0:
+            print("Organism A will take", total_weeks, "weeks until species reaches 0 organisms. ")
+            break
+
+        weeks += 1
+        total_weeks += 1
+
+
+organism_a()
