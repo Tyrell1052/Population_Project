c905d76c74d04da0f09f84c15eced153f3056358
Tyrell <Tyrell1052@gmail.com>
2018-10-08 03:20:25 -0400

    trying to add one class for organism to make instances of organism a, b and c.

--- test2.py
+++ test2.py
@@ -1,43 +1,50 @@
-def organism(a,b,c):
-    print("%4s%14s%14s%14s" %
-          ("Year:", "Organism A:", "Organism B:", "Organism C"))
 
-    population = 50
-    weeks = 0
-    total_weeks = 0
-    year = 0
+class Organism:
 
-    while total_weeks < 260:
+    def __init__(self, population):
+        print("%4s%14s%14s%14s" %
+              ("Year:", "Organism A:", "Organism B:", "Organism C"))
 
-        if total_weeks == 52 or 104 or 156 or 208 or 260:
-            year += 1
-        else:
-            break
+        self.population = population
+        self.weeks = 0
+        self.total_weeks = 0
+        self.year = 0
 
+        while self.total_weeks < 260:
 
-        if weeks == 2:
-            population = population - (population * .25)
-
-        elif weeks == 4:
-            population = population * 2
-            print("%12s" % format(population, ".0f"))
-            return
+            if self.total_weeks == 52 or 104 or 156 or 208 or 260:
+                self.year += 1
+            else:
+                break
 
 
-        elif population > 1000000:
-            print("Organism A will take", total_weeks, "weeks to reach 1 million organisms.")
-            break
+            if self.weeks == 2:
+                self.population = self.population - (self.population * .25)
 
-        elif population == 0:
-            print("Organism A will take", total_weeks, "weeks until species reaches 0 organisms. ")
-            break
-        weeks += 1
-        total_weeks += 1
+            elif self.weeks == 4:
+                self.population = self.population * 2
+                print("%12s" % format(self.population, ".0f"))
+                return
 
-        if year <= 5:
-            print("%0s" % year)
-        else:
-            break
-    return
 
-organism(1,2,c)
+            elif self.population > 1000000:
+                print("Organism A will take", self.total_weeks, "weeks to reach 1 million organisms.")
+                break
+
+            elif self.population == 0:
+                print("Organism A will take", self.total_weeks, "weeks until species reaches 0 organisms. ")
+                break
+            self.weeks += 1
+            self.total_weeks += 1
+
+            if self.year <= 5:
+                print("%0s" % self.year)
+            else:
+                break
+        return
+
+
+#Organism("Organism A", "Organism B","Organism C")
+
+Organism_A = Organism(50)
+Organism_B = Organism(55)
