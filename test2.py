868c6ce90a48323b6ec8c85335aff5d3e759411b
Tyrell <Tyrell1052@gmail.com>
2018-10-08 02:55:15 -0400

    trying to add one function for organism a, b, and c.

--- test2.py
+++ test2.py
@@ -1,28 +1,43 @@
-def organism_b():
+def organism(a,b,c):
+    print("%4s%14s%14s%14s" %
+          ("Year:", "Organism A:", "Organism B:", "Organism C"))
 
-    population = 250
+    population = 50
     weeks = 0
     total_weeks = 0
+    year = 0
 
     while total_weeks < 260:
+
+        if total_weeks == 52 or 104 or 156 or 208 or 260:
+            year += 1
+        else:
+            break
+
 
         if weeks == 2:
             population = population - (population * .25)
 
         elif weeks == 4:
-            population = population * 3
-            print("%14s" % format(population, ".0f"))
-            weeks = 0
+            population = population * 2
+            print("%12s" % format(population, ".0f"))
+            return
+
+
         elif population > 1000000:
-            print("Organism B will take", total_weeks, "weeks to reach 1 million organisms.")
+            print("Organism A will take", total_weeks, "weeks to reach 1 million organisms.")
             break
-        elif population < 0:
-            print("Organism B will take", total_weeks, "weeks until species reaches 0 organisms. ")
+
+        elif population == 0:
+            print("Organism A will take", total_weeks, "weeks until species reaches 0 organisms. ")
             break
         weeks += 1
         total_weeks += 1
 
-        #print("%0s" % total_weeks)
+        if year <= 5:
+            print("%0s" % year)
+        else:
+            break
+    return
 
-
-organism_b()
+organism(1,2,c)
