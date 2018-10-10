3cbb5718a75a6e11b2d81ba00a78487bff6e493b
Tyrell1052 <tyrell1052@gmail.com>
2018-10-10 02:21:15 -0400

    trying to get program to print years and population each year until 5 years

--- T.py
+++ T.py
@@ -1,9 +1,12 @@
-
 def organism_b():
-
     # declaring population and week_counter variables
     population = 250
     week_counter = 0
+    year_1 = 52
+    year_2 = 104
+
+
+
 
     # this will start the while loop to run until total_weeks is <= 260 weeks (5 years if counted in weeks)
     while week_counter <= 260:
@@ -12,14 +15,18 @@ def organism_b():
             population = population - (population * .25)
             week_counter += 1
 
-
             if week_counter % 4 == 0:  # this is for when weeks == 4 the population will increase
                 population = population * 3
                 week_counter += 1
 
+        if week_counter == 52 or 104 or 156 or 208 or 260:
+            print(population, "at", week_counter)
+
         if population > 1000000 or population < 1:
             print("Organism B will reach", format(population, ".0f"), "by week", week_counter)
             break
-        #print(population)
+        # print(population)
+
+
 
 organism_b()
