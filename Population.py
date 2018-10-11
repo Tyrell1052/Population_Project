e3c42fae176b6cc52f97feb0ae0b2439bcbcc2df
Tyrell1052 <tyrell1052@gmail.com>
2018-10-10 23:12:58 -0400

    completed the main population file

--- Population.py
+++ Population.py
@@ -6,74 +6,89 @@ based on growth rates, death rates and predictions over
 the course of 5 years """
 
 
-# Display headers for table
-
-# define Organism A in a function
 def organism_a():
-    # Display headers for table
-    print("%4s%14s" %
-          ("Year:", "Organism A:"))
-    # starting variables for population, weeks and total_weeks
-    population = 50
-    weeks = 0
-    total_weeks = 0
-    # this will start the while loop to run until total_weeks is <= 260 weeks (5 years if counted in weeks)
-    while total_weeks <= 260:
 
-        if weeks == 2:  # this if statement handles the decrease by 25% every two weeks
+    # declaring population and week_counter variables
+    population = 50
+    week_counter = 0
+
+    # this will start the while loop to run until weeks_counter is <= 260 weeks
+    # (5 years if counted in weeks)
+    while week_counter <= 260:
+        week_counter += 1
+        if week_counter % 2 == 0:  # this if statement handles the decrease by 25% every two weeks
             population = population - (population * .25)
 
-        elif weeks == 4:  # this is for when weeks == 4 the population will increase
-            population = population * 2
-            print("%12s" % format(population, ".0f"))
-            weeks = 0  #this sets weeks back to 0 so when the loop continues to go through it can decrease and increase the population
+            if week_counter % 4 == 0:  # this is for when weeks == 4 the population will increase
+                population = population * 2
 
-        elif population > 1000000:  # this will print only if the population is greater then 1000000
-            print("Organism A will take", total_weeks, "weeks to reach 1 million organisms.")
-            break
-        elif population < 0:  # this will print if the population is less than 0
-            print("Organism A will take", total_weeks, "weeks until species reaches 0 organisms. ")
+        if week_counter == 52 or week_counter == 104 or week_counter == 156 or week_counter == 208 or week_counter ==260:
+            print("Organisms A population", format(population, ".0f"), "at year", week_counter // 52)
+
+        if population > 1000000 or population < 1:
+            print("Organism A will reach", format(population, ".0f"), "by week", week_counter)
             break
 
-        weeks += 1  # this adds 1 to weeks each time the while loop iterates
-        total_weeks += 1  # this adds 1 to weeks each time the while loop iterates
 
-        print("%0s" % total_weeks)
-
- #---------------------------------------------------------------------------------------
 
 def organism_b():
-
-    print("%4s%14s" %
-          ("Week:", "Organism B:"))
-
+    # declaring population and week_counter variables
     population = 250
-    weeks = 0
-    total_weeks = 0
+    week_counter = 0
 
-    while total_weeks < 260:
 
-        if weeks == 2:
+
+    # this will start the while loop to run until total_weeks is <= 260 weeks
+    #(5 years if counted in weeks)
+    while week_counter <= 260:
+        week_counter += 1
+        if week_counter % 1 == 0:  # this if statement handles the decrease by 25% every two weeks
             population = population - (population * .25)
 
-        elif weeks == 4:
-            population = population * 3
-            print("%12s" % format(population, ".0f"))
-            weeks = 0
-        elif population > 1000000:
-            print("Organism B will take", total_weeks, "weeks to reach 1 million organisms.")
+            if week_counter % 4 == 0:  # this is for when weeks == 4 the population will increase
+                population = population * 3
+
+        if week_counter == 52 or week_counter == 104 or week_counter == 156 or week_counter == 208 or week_counter ==260:
+            print("Organisms B population", format(population, ".0f"), "at year", week_counter // 52)
+
+        if population > 1000000 or population < 1:
+            print("Organism B will reach", format(population, ".0f"), "by week", week_counter)
             break
-        elif population < 0:
-            print("Organism B will take", total_weeks, "weeks until species reaches 0 organisms. ")
+
+
+
+
+
+def organism_c():
+
+    # declaring population and week_counter variables
+    population = 1000
+    week_counter = 0
+
+    # this will start the while loop to run until total_weeks is <= 260 weeks (5 years if counted in weeks)
+    while week_counter <= 260:
+        week_counter += 1
+        if week_counter % 1 == 0:  # this if statement handles the decrease by 25% every two weeks
+            population = population - (population * .13)
+
+            if week_counter % 4 == 0:  # this is for when weeks == 4 the population will increase
+                population = population + (population * .75)
+
+        if week_counter == 52 or week_counter == 104 or week_counter == 156 or week_counter == 208 or week_counter ==260:
+            print("Organisms C population", format(population, ".0f"), "at year", week_counter // 52)
+
+        if population > 1000000 or population < 1:
+            print("Organism C will reach", format(population, ".0f"), "by week", week_counter)
             break
-        weeks += 1
-        total_weeks += 1
-
-        print("%0s" % total_weeks)
 
 
+# this is where I will call each function created.
 
+print("Organism A's population over 5 years:")
+print(organism_a())
 
+print("Organism B's population over 5 years:")
+print(organism_b())
 
-
-print(organism_a(),organism_b())
+print("Organism C's population over 5 years:")
+print(organism_c())
